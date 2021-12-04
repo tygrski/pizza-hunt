deb0d42aef209f5fc782230fc0b162aa9eca58d3
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-03 19:46:12 -0600

    syntaxerr

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,6 +9,20 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  // get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  // get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(printPizza);
+}
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -87,7 +101,30 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      // location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
+
 
 function handleNewReplySubmit(event) {
   event.preventDefault();
@@ -114,3 +151,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
